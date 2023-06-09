810035394280fa8400f733e4a388de5a26d1be23
issblann <anaisabelpp@outlook.com>
2023-06-08 20:48:54 -0500

    add filters

--- ClientsCrud/src/components/ClientsList/ClientList.jsx
+++ ClientsCrud/src/components/ClientsList/ClientList.jsx
@@ -4,17 +4,33 @@ import { getAllClients } from "../../api/client.api";
 import ClientsCard from "../ClientsCard/ClientsCard";
 import { Link } from "react-router-dom";
 import { ClientContext } from "../../context/Context";
+import { useForm } from "react-hook-form";
 
 const ClientList = () => {
   const { clients, setClients, deleteClient, editClient, resetValue } =
     useContext(ClientContext);
+  const { register, handleSubmit } = useForm();
+
   useEffect(() => {
     async function loadClients() {
       const res = await getAllClients();
       setClients(res.data);
     }
     loadClients();
-  }, [clients]);
+  }, []);
+
+  let data = [];
+  data = clients.filter((client) => client.name.toLowerCase().includes(""));
+
+  const submit = (e) => {
+    data = clients.filter(
+      (client) =>
+        client.birth_date.includes(e.target.value) ||
+        client.name.toLowerCase().includes(e.target.value)
+    );
+    setClients(data);
+    console.log(data);
+  };
 
   return (
     <section className="w-full mt-8 flex flex-col bg-slate-500 px-4 py-3 items-center justify-center">
@@ -36,6 +52,7 @@ const ClientList = () => {
         <form className="  flex items-center p-3 gap-2 border">
           <AiOutlineSearch type="submit" className=" text-gray-400" />
           <input
+            onChange={submit}
             className="w-full placeholder:text-gray-400 focus:outline-none bg-transparent"
             type="text"
             placeholder="Search..."
@@ -43,7 +60,7 @@ const ClientList = () => {
         </form>
 
         <div>
-          {clients.map((client) => {
+          {data.map((client) => {
             return <ClientsCard key={client.id} client={client} />;
           })}
         </div>
